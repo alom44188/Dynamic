70b6558536e5dc088a466a941c4e9714f8a53c24
alom44188 <79210994+alom44188@users.noreply.github.com>
2021-04-09 01:29:47 +0600

    Add files via upload

---
+++ Blog Theme/sidebar.php
@@ -0,0 +1,18 @@
+
+ <?php dynamic_sidebar(); ?>
+
+<div class="treading">
+
+        
+                    <div class="ttle">
+                        <h2>treading</h2>
+                    </div>
+                    <a href="">Radical new Apple AirPods 3 redesign revealed</a><br>
+                    <a href="">Google's new tool will help you build a knockout website</a><br>
+                    <a href="">New Apple M1 MacBook Pro surprise price cut returns</a><br>
+                    <a href="">How Shopify gives your eCommerce clients the speed they need
+                    </a><br>
+                    <a href="">10 reasons you should build your next website with Editor X</a><br>
+
+                </div>
+               
